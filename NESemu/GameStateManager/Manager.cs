e2437c059bda87cf13c938bec8b4a69479f6eb76
Earlopain <earlopain@c5h8no4na.net>
2018-07-21 21:54:12 +0200

    Track all things

---
+++ NESemu/GameStateManager/Manager.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+using Camera;
+using Layers;
+using Level;
+
+namespace GameState
+{
+    public class GameStateManager
+    {
+        CameraManager Camera;
+        LayerManager LayerManager;
+        ContentManager Content;
+
+        public GameStateManager(GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            this.LayerManager = new LayerManager(graphicsDevice, content);
+            this.Camera = new CameraManager(graphicsDevice, 0, 1);
+            this.Content = content;
+            LevelParser.MakeTemp(LayerManager, Content);
+        }
+
+        public void Draw()
+        {
+            LayerManager.Draw(Camera);
+            return;
+        }
+
+        public void ApplyKeyState(KeyboardState keyState)
+        {
+            Camera.ApplyKeyState(keyState);
+        }
+
+    }
+}
