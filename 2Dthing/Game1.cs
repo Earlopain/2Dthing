e365cddfc39d2160f93a969a836854f73f948c80
Earlopain <earlopain@c5h8no4na.net>
2018-07-22 22:04:30 +0200

    Do not consider only keyboard input

--- 2Dthing/Game1.cs
+++ 2Dthing/Game1.cs
@@ -66,7 +66,7 @@ namespace Thing
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            GameStateManager.ApplyKeyState(Keyboard.GetState());
+            GameStateManager.ApplyUserInput(Keyboard.GetState(), Mouse.GetState());
 
             // TODO: Add your update logic here
 
