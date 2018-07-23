aec0d0d49a828aa6c51a622471fa0e586c9e48b9
Earlopain <earlopain@c5h8no4na.net>
2018-07-23 15:26:17 +0200

    Uniform call for Static and Animated Sprite, cleanup

--- 2Dthing/LayerManager/Util.cs
+++ 2Dthing/LayerManager/Util.cs
@@ -1,12 +1,12 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Layers
 {
     public static class Util
     {
-        public static Point getDrawingOffset(Rectangle drawArea, Rectangle camera)
+        public static Point getDrawingOffset(Rectangle drawArea, Rectangle camera, LocationOverlap overlap)
         {
-            LocationOverlap overlap = getOverlapLocation(drawArea, camera);
             switch (overlap)
             {
                 case LocationOverlap.TopLeft:
@@ -30,9 +30,8 @@ namespace Layers
             }
         }
 
-        public static Rectangle getDrawingRectangle(Rectangle drawArea, Rectangle camera, Point offset)
+        public static Rectangle getDrawingRectangle(Rectangle drawArea, Rectangle camera, Point offset, LocationOverlap overlap)
         {
-            LocationOverlap overlap = getOverlapLocation(drawArea, camera);
             switch (overlap)
             {
                 case LocationOverlap.TopLeft:
@@ -56,30 +55,47 @@ namespace Layers
             }
         }
 
-        public static Rectangle getTexurePart(Point offset, Rectangle area, Rectangle camera)
+        public static Rectangle getTexurePart(Rectangle area, Rectangle camera, Point offset, LocationOverlap overlap)
         {
-            LocationOverlap overlap = getOverlapLocation(area, camera);
+            return getTexurePart(area, camera, offset, overlap, new Rectangle(0, 0, area.Width, area.Height));
+        }
+
+        public static Rectangle getTexurePart(Rectangle area, Rectangle camera, Point offset, LocationOverlap overlap, Rectangle sourceRectangle)
+        {
+            Rectangle result;
             switch (overlap)
             {
                 case LocationOverlap.TopLeft:
-                    return new Rectangle(offset.X, offset.Y, area.Width - offset.X, area.Height - offset.Y);
+                    result = new Rectangle(offset.X, offset.Y, area.Width - offset.X, area.Height - offset.Y);
+                    break;
                 case LocationOverlap.TopRight:
-                    return new Rectangle(0, offset.Y, area.Width - offset.X, area.Height - offset.Y);
+                    result = new Rectangle(0, offset.Y, area.Width - offset.X, area.Height - offset.Y);
+                    break;
                 case LocationOverlap.BottomLeft:
-                    return new Rectangle(offset.X, 0, area.Width - offset.X, area.Height - offset.Y);
+                    result = new Rectangle(offset.X, 0, area.Width - offset.X, area.Height - offset.Y);
+                    break;
                 case LocationOverlap.BottomRight:
-                    return new Rectangle(0, 0, area.Width - offset.X, area.Height - offset.Y);
+                    result = new Rectangle(0, 0, area.Width - offset.X, area.Height - offset.Y);
+                    break;
                 case LocationOverlap.TopOnly:
-                    return new Rectangle(0, offset.Y, area.Width, area.Height - offset.Y);
+                    result = new Rectangle(0, offset.Y, area.Width, area.Height - offset.Y);
+                    break;
                 case LocationOverlap.LeftOnly:
-                    return new Rectangle(offset.X, 0, area.Width - offset.X, area.Height);
+                    result = new Rectangle(offset.X, 0, area.Width - offset.X, area.Height);
+                    break;
                 case LocationOverlap.BottomOnly:
-                    return new Rectangle(0, 0, area.Width, area.Height - offset.Y);
+                    result = new Rectangle(0, 0, area.Width, area.Height - offset.Y);
+                    break;
                 case LocationOverlap.RightOnly:
-                    return new Rectangle(0, 0, area.Width - offset.X, area.Height);
+                    result = new Rectangle(0, 0, area.Width - offset.X, area.Height);
+                    break;
                 default:
-                    return new Rectangle(0, 0, area.Width, area.Height);
+                    result = new Rectangle(0, 0, area.Width, area.Height);
+                    break;
             }
+            //Apply the Texture offset if texture is multiframe
+            result.Offset(sourceRectangle.Location);
+            return result;
         }
 
         public static LocationOverlap getOverlapLocation(Rectangle drawArea, Rectangle camera)
