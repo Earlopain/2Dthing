0c0cba701c1bc3d33433c6fc97c7f71ab172b6da
Earlopain <earlopain@c5h8no4na.net>
2018-07-23 13:48:25 +0200

    Hopefully a bit more clear code

---
+++ 2Dthing/LayerManager/Overlap.cs
@@ -0,0 +1,15 @@
+namespace Layers
+{
+    public enum LocationOverlap
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+        TopOnly,
+        BottomOnly,
+        LeftOnly,
+        RightOnly,
+        None
+    }
+}
--- 2Dthing/LayerManager/Util.cs
+++ 2Dthing/LayerManager/Util.cs
@@ -4,115 +4,103 @@ namespace Layers
 {
     public static class Util
     {
-        public static Point getDrawingOffset(Rectangle area, Rectangle drawArea)
-        {   //top left
-            if (area.Top < drawArea.Top && area.Left < drawArea.Left)
+        public static Point getDrawingOffset(Rectangle drawArea, Rectangle camera)
+        {
+            LocationOverlap overlap = getOverlapLocation(drawArea, camera);
+            switch (overlap)
             {
-                return new Point(drawArea.Left - area.Left, drawArea.Top - area.Top);
-            }//top right
-            else if (area.Top < drawArea.Top && area.Right > drawArea.Right)
-            {
-                return new Point(area.Right - drawArea.Right, drawArea.Top - area.Top);
-            }//bottom left
-            else if (area.Bottom > drawArea.Bottom && area.Left < drawArea.Left)
-            {
-                return new Point(drawArea.Left - area.Left, area.Bottom - drawArea.Bottom);
-            }//botom right
-            else if (area.Bottom > drawArea.Bottom && area.Right > drawArea.Right)
-            {
-                return new Point(area.Right - drawArea.Right, area.Bottom - drawArea.Bottom);
-            }//top only
-            else if (area.Top < drawArea.Top)
-            {
-                return new Point(0, drawArea.Top - area.Top);
-            }//left only
-            else if (area.Left < drawArea.Left)
-            {
-                return new Point(drawArea.Left - area.Left, 0);
-            }//bottom only
-            else if (area.Bottom > drawArea.Bottom)
-            {
-                return new Point(0, area.Bottom - drawArea.Bottom);
-            }//right only
-            else if (area.Right > drawArea.Right)
-            {
-                return new Point(area.Right - drawArea.Right, 0);
+                case LocationOverlap.TopLeft:
+                    return new Point(camera.Left - drawArea.Left, camera.Top - drawArea.Top);
+                case LocationOverlap.TopRight:
+                    return new Point(drawArea.Right - camera.Right, camera.Top - drawArea.Top);
+                case LocationOverlap.BottomLeft:
+                    return new Point(camera.Left - drawArea.Left, drawArea.Bottom - camera.Bottom);
+                case LocationOverlap.BottomRight:
+                    return new Point(drawArea.Right - camera.Right, drawArea.Bottom - camera.Bottom);
+                case LocationOverlap.TopOnly:
+                    return new Point(0, camera.Top - drawArea.Top);
+                case LocationOverlap.LeftOnly:
+                    return new Point(camera.Left - drawArea.Left, 0);
+                case LocationOverlap.BottomOnly:
+                    return new Point(0, drawArea.Bottom - camera.Bottom);
+                case LocationOverlap.RightOnly:
+                    return new Point(drawArea.Right - camera.Right, 0);
+                default:
+                    return Point.Zero;
             }
-            return Point.Zero;
         }
 
         public static Rectangle getDrawingRectangle(Rectangle drawArea, Rectangle camera, Point offset)
         {
-            if (drawArea.Top < camera.Top && drawArea.Left < camera.Left)
+            LocationOverlap overlap = getOverlapLocation(drawArea, camera);
+            switch (overlap)
             {
-                return new Rectangle(drawArea.Location.X + offset.X, drawArea.Location.Y + offset.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
-            }//top right
-            else if (drawArea.Top < camera.Top && drawArea.Right > camera.Right)
-            {
-                return new Rectangle(drawArea.Location.X, drawArea.Location.Y + offset.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
-            }//bottom left
-            else if (drawArea.Bottom > camera.Bottom && drawArea.Left < camera.Left)
-            {
-                return new Rectangle(drawArea.Location.X + offset.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
-            }//botom right
-            else if (drawArea.Bottom > camera.Bottom && drawArea.Right > camera.Right)
-            {
-                return new Rectangle(drawArea.Location.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
-            }//top only
-            else if (drawArea.Top < camera.Top)
-            {
-                return new Rectangle(drawArea.Location.X, drawArea.Location.Y + offset.Y, drawArea.Width, drawArea.Height - offset.Y);
-            }//left only
-            else if (drawArea.Left < camera.Left)
-            {
-                return new Rectangle(drawArea.Location.X + offset.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height);
-            }//bottom only
-            else if (drawArea.Bottom > camera.Bottom)
-            {
-                return new Rectangle(drawArea.Location.X, drawArea.Location.Y, drawArea.Width, drawArea.Height - offset.Y);
-            }//right only
-            else if (drawArea.Right > camera.Right)
-            {
-                return new Rectangle(drawArea.Location.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height);
+                case LocationOverlap.TopLeft:
+                    return new Rectangle(drawArea.Location.X + offset.X, drawArea.Location.Y + offset.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
+                case LocationOverlap.TopRight:
+                    return new Rectangle(drawArea.Location.X, drawArea.Location.Y + offset.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
+                case LocationOverlap.BottomLeft:
+                    return new Rectangle(drawArea.Location.X + offset.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
+                case LocationOverlap.BottomRight:
+                    return new Rectangle(drawArea.Location.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height - offset.Y);
+                case LocationOverlap.TopOnly:
+                    return new Rectangle(drawArea.Location.X, drawArea.Location.Y + offset.Y, drawArea.Width, drawArea.Height - offset.Y);
+                case LocationOverlap.LeftOnly:
+                    return new Rectangle(drawArea.Location.X + offset.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height);
+                case LocationOverlap.BottomOnly:
+                    return new Rectangle(drawArea.Location.X, drawArea.Location.Y, drawArea.Width, drawArea.Height - offset.Y);
+                case LocationOverlap.RightOnly:
+                    return new Rectangle(drawArea.Location.X, drawArea.Location.Y, drawArea.Width - offset.X, drawArea.Height);
+                default:
+                    return drawArea;
             }
-            return Rectangle.Empty;
         }
 
         public static Rectangle getTexurePart(Point offset, Rectangle area, Rectangle camera)
         {
-            if (area.Top < camera.Top && area.Left < camera.Left)
+            LocationOverlap overlap = getOverlapLocation(area, camera);
+            switch (overlap)
             {
-                return new Rectangle(offset.X, offset.Y, area.Width - offset.X, area.Height - offset.Y);
-            }//top right
-            else if (area.Top < camera.Top && area.Right > camera.Right)
-            {
-                return new Rectangle(0, offset.Y, area.Width - offset.X, area.Height - offset.Y);
-            }//bottom left
-            else if (area.Bottom > camera.Bottom && area.Left < camera.Left)
-            {
-                return new Rectangle(offset.X, 0, area.Width - offset.X, area.Height - offset.Y);
-            }//botom right
-            else if (area.Bottom > camera.Bottom && area.Right > camera.Right)
-            {
-                return new Rectangle(0, 0, area.Width - offset.X, area.Height - offset.Y);
-            }//top only
-            else if (area.Top < camera.Top)
-            {
-                return new Rectangle(0, offset.Y, area.Width, area.Height - offset.Y);
-            }//left only
-            else if (area.Left < camera.Left)
-            {
-                return new Rectangle(offset.X, 0, area.Width - offset.X, area.Height);
-            }//bottom only
-            else if (area.Bottom > camera.Bottom)
-            {
-                return new Rectangle(0, 0, area.Width, area.Height - offset.Y);
-            }//right only
-            else if (area.Right > camera.Right)
-            {
-                return new Rectangle(0, 0, area.Width - offset.X, area.Height);
+                case LocationOverlap.TopLeft:
+                    return new Rectangle(offset.X, offset.Y, area.Width - offset.X, area.Height - offset.Y);
+                case LocationOverlap.TopRight:
+                    return new Rectangle(0, offset.Y, area.Width - offset.X, area.Height - offset.Y);
+                case LocationOverlap.BottomLeft:
+                    return new Rectangle(offset.X, 0, area.Width - offset.X, area.Height - offset.Y);
+                case LocationOverlap.BottomRight:
+                    return new Rectangle(0, 0, area.Width - offset.X, area.Height - offset.Y);
+                case LocationOverlap.TopOnly:
+                    return new Rectangle(0, offset.Y, area.Width, area.Height - offset.Y);
+                case LocationOverlap.LeftOnly:
+                    return new Rectangle(offset.X, 0, area.Width - offset.X, area.Height);
+                case LocationOverlap.BottomOnly:
+                    return new Rectangle(0, 0, area.Width, area.Height - offset.Y);
+                case LocationOverlap.RightOnly:
+                    return new Rectangle(0, 0, area.Width - offset.X, area.Height);
+                default:
+                    return new Rectangle(0, 0, area.Width, area.Height);
             }
-            return Rectangle.Empty;
+        }
+
+        public static LocationOverlap getOverlapLocation(Rectangle drawArea, Rectangle camera)
+        {
+            if (drawArea.Top < camera.Top && drawArea.Left < camera.Left)
+                return LocationOverlap.TopLeft;
+            else if (drawArea.Top < camera.Top && drawArea.Right > camera.Right)
+                return LocationOverlap.TopRight;
+            else if (drawArea.Bottom > camera.Bottom && drawArea.Left < camera.Left)
+                return LocationOverlap.BottomLeft;
+            else if (drawArea.Bottom > camera.Bottom && drawArea.Right > camera.Right)
+                return LocationOverlap.BottomRight;
+            else if (drawArea.Top < camera.Top)
+                return LocationOverlap.TopOnly;
+            else if (drawArea.Left < camera.Left)
+                return LocationOverlap.LeftOnly;
+            else if (drawArea.Bottom > camera.Bottom)
+                return LocationOverlap.BottomOnly;
+            else if (drawArea.Right > camera.Right)
+                return LocationOverlap.RightOnly;
+            return LocationOverlap.None;
         }
     }
 }
